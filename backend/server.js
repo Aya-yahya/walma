eee1cc11c9eedd501775c210e4289538af44925d
unknown <ayameshref27@gmail.com>
2021-02-21 12:36:27 +0300

    Prepare for deployment

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  5000,
+  console.log(`server running on port ${process.env.PORT}`.yellow.bold)
+)

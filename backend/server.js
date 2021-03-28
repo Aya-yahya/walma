12356979553176fce5c3d092479ea11e6b0b8b24
unknown <ayameshref27@gmail.com>
2021-03-28 05:25:23 +0300

    React setup

--- backend/server.js
+++ backend/server.js
@@ -40,22 +40,18 @@ app.use('/api/orders', orderRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) //this makes the uploads folder static. important for images.
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+)
+/*if (process.env.NODE_ENV === 'production') {
 } else {
   app.get('/', (req, res) => {
     res.send('API is running')
   })
-}
+}*/
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-app.listen(
-  PORT,
-  console.log(`server running on port ${process.env.PORT}`.yellow.bold)
-)
